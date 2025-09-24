10b9a14a614be73c4c4345037ecdcd5735370bf0
tatata812 <75353660+tatata812@users.noreply.github.com>
2025-09-25 01:39:37 +0900

    途中

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,5 @@
 $(function () {
 
-  // SPメニュー
-  $("#js-hamburger-menu, .navigation__link").click(function () {
-    $(".header-sp,.black-bg,.header").toggleClass("active"); //ボタン自身に activeクラスを付与し
-  });
-  $(".header-sp__btn-img").click(function () {
-    $(".header-sp,.black-bg,.header").toggleClass("active");
-    $('.hamburger-menu').toggleClass('hamburger-menu--open');
-  });
-
-  $('#js-hamburger-menu, .navigation__link').on('click', function () {
-    $('.navigation').slideToggle(500);
-    $('.hamburger-menu').toggleClass('hamburger-menu--open')
-  });
-
 
   // TOPへボタン
   $(function () {
@@ -44,53 +30,33 @@ $(function () {
     });
   });
 
-
-  // メインビジュアル　スライダー
-  $(document).ready(function () {
-    const $slides = $('.main-visual-js img');
-    let current = 0;
-    const slideCount = $slides.length;
-    const intervalTime = 4000; // 3秒ごとに切り替え
-
-    // 初期設定：最初の画像以外を非表示
-    $slides.hide().eq(current).show();
-
-    // スライド切り替え
-    setInterval(function () {
-      const next = (current + 1) % slideCount;
-      $slides.eq(current).fadeOut(1000);
-      $slides.eq(next).fadeIn(1000);
-      current = next;
-    }, intervalTime);
-  });
-
-
   //フェードイン
-  $(window).scroll(function () {
-    const windowHeight = $(window).height(); //ウィンドウの高さ
-    const scroll = $(window).scrollTop(); //スクロール量
+$(function() {
+  function fadeInOnScroll() {
+    $('.fade-in-up, .fade-in-right, .fade-in-zoom,.fade-in-left').each(function() {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
 
-    $(".fade-in-js").each(function () {
-      const targetPosition = $(this).offset().top; //要素の上からの距離
-      if (scroll > targetPosition - windowHeight + 100) {
-        $(this).addClass("action");
+      if (scroll > elemPos - windowHeight + 100) {
+        if ($(this).hasClass('fade-in-zoom')) {
+          // zoomだけ遅延
+          var el = $(this);
+          setTimeout(function(){
+            el.addClass('action');
+          }, 300); // 0.3秒遅延
+        } else {
+          $(this).addClass('action');
+        }
       }
     });
+  }
+
+  fadeInOnScroll();
+
+  $(window).on('scroll', function() {
+    fadeInOnScroll();
   });
-
-
-  $(function () {
-    var headerHeight = 40; // ヘッダーの高さ
-    $('a[href^="#"]').click(function () {
-      var speed = 500;
-      var href = $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top - headerHeight;
-      $("html, body").animate({
-        scrollTop: position
-      }, speed, "swing");
-      return false;
-    });
-  });
-
+});
+  
 })
