a8887e4a4f11a6928980e5916da6f6cd2d76d0e8
tatata812 <75353660+tatata812@users.noreply.github.com>
2025-09-25 18:18:16 +0900

    トップボタン設置

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,35 +30,56 @@ $(function () {
     });
   });
 
-// フェードイン
-$(function() {
-  function fadeInOnScroll() {
-    $('.fade-in-up, .fade-in-right, .fade-in-zoom, .fade-in-left').each(function() {
-      var elemPos = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var windowHeight = $(window).height();
+  // フェードイン
+  $(function () {
+    function fadeInOnScroll() {
+      $('.fade-in-up, .fade-in-right, .fade-in-zoom, .fade-in-left').each(function () {
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
 
-      if (scroll > elemPos - windowHeight + 100) {
-        // すべて同じ処理（即時 action 付与）
-        $(this).addClass('action');
+        if (scroll > elemPos - windowHeight + 100) {
+          // すべて同じ処理（即時 action 付与）
+          $(this).addClass('action');
+        }
+      });
+    }
+
+    fadeInOnScroll();
+
+    $(window).on('scroll', function () {
+      fadeInOnScroll();
+    });
+  });
+
+  $(function () {
+    $(".faq-question").on("click", function () {
+      var parent = $(this).closest(".faq-item");
+      parent.toggleClass("open"); // 矢印だけ切り替え
+      parent.find(".faq-answer").stop().slideToggle(300); // 開閉はここでだけ制御
+    });
+  });
+
+
+  // トップへ戻る
+  $(function () {
+    var $pageTop = $("#page-top");
+
+    $(window).on("scroll", function () {
+      if ($(this).scrollTop() > 300) {
+        $pageTop.addClass("show");
+      } else {
+        $pageTop.removeClass("show");
       }
     });
-  }
 
-  fadeInOnScroll();
-
-  $(window).on('scroll', function() {
-    fadeInOnScroll();
+    $pageTop.on("click", function () {
+      $("html, body").animate({
+        scrollTop: 0
+      }, 500);
+      return false;
+    });
   });
-});
 
-$(function() {
-  $(".faq-question").on("click", function() {
-    var parent = $(this).closest(".faq-item");
-    parent.toggleClass("open"); // 矢印だけ切り替え
-    parent.find(".faq-answer").stop().slideToggle(300); // 開閉はここでだけ制御
-  });
-});
 
-  
 })
